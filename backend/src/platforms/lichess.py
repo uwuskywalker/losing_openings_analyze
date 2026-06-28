8097bb94feb9088a9cc02571eb3d2ddbede17a79
uwuskywalker <alan25555391@gmail.com>
2026-06-28 09:37:42 +0000

    fix: 更新資料庫連接密碼環境變數名稱為 DB_PASSWORD

--- backend/src/platforms/lichess.py
+++ backend/src/platforms/lichess.py
@@ -52,7 +52,7 @@ class LichessPlatform(ChessPlatform):
             conn = psycopg2.connect(
                 dbname=os.getenv("DB_NAME"),
                 user=os.getenv("DB_USER"),
-                password=os.getenv("DB_PASS"),
+                password=os.getenv("DB_PASSWORD"),
                 host=os.getenv("DB_HOST"),
                 port=os.getenv("DB_PORT")
             )
